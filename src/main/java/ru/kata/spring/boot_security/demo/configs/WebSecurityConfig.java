72aeb85c5482ae70f819eb04a5d1c0cdbe59f322
Mariya <bitvinka@gmail.com>
2024-02-04 14:49:43 +0300

    settings

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -25,13 +25,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                    .csrf().disable()
+        http.csrf().disable()
                     .authorizeRequests()
+                    .antMatchers("/", "/error", "/resources/**", "/static/**").permitAll()
                     .antMatchers("/admin/**","/api/admin/**").hasRole("ADMIN")
                     .antMatchers("/api/user/**").hasAnyRole("USER", "ADMIN")
-                    .antMatchers("/", "/error", "/index", "/registration", "/resources/**", "/static/**").permitAll()
-                    .anyRequest().hasAnyRole("USER", "ADMIN")
+                    .anyRequest().authenticated()
                 .and()
                     .formLogin().loginPage("/login")
                     .loginProcessingUrl("/process_login")
